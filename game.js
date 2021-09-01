1fb1a410aaa224fc09334efe1a49d0aab5237785
kaczenka <75174671+kaczenka@users.noreply.github.com>
2021-09-01 13:50:13 +0200

    collision fix

--- game.js
+++ game.js
@@ -1,11 +1,12 @@
 //canvas
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
+
 //player
 const snakeSize = 50;
-let snakeSpeed = 50;
+let snakeSpeed = snakeSize;
 let snakePosX = 0;
-let snakePosY = canvas.height / 2 - snakeSize / 2;
+let snakePosY = canvas.height / 2;
 
 let velocityX = 0;
 let velocityY = 0;
@@ -29,16 +30,16 @@ function moveStuff() {
     snakePosX += snakeSpeed * velocityX;
     snakePosY += snakeSpeed * velocityY;
 
-    if (snakePosX > canvas.width) {
+    if (snakePosX > (canvas.width - snakeSize)) {
         snakePosX = 0;
     }
-    if (snakePosX < -snakeSize) {
+    if (snakePosX < 0) {
         snakePosX = canvas.width;
     }
-    if (snakePosY > canvas.height) {
+    if (snakePosY > (canvas.height - snakeSize)) {
         snakePosY = 0;
     }
-    if (snakePosY < -snakeSize) {
+    if (snakePosY < 0) {
         snakePosY = canvas.height;
     }
 }
